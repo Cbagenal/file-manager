4ad361d84aa2331effd7f0f481d2228389fe474e
Callum Bagenal <cb@Callums-MacBook-Pro.local>
2026-07-17 19:10:50 +0100

    convex schema added

--- convex/schema.ts
+++ convex/schema.ts
@@ -7,7 +7,22 @@ import { authTables } from "@convex-dev/auth/server";
 // The schema provides more precise TypeScript types.
 export default defineSchema({
   ...authTables,
-  numbers: defineTable({
-    value: v.number(),
+
+  files: defineTable({
+    ownerId: v.id("users"),
+    folderId: v.id("folders"),
+    type: v.string(),
+    name: v.string(),
+    size: v.number(),
+    dateCreated: v.number(),
+    uploadThingUrl: v.string()
   }),
+
+  folders: defineTable({
+    name: v.string(),
+    parentFolderId: v.id("folders"),
+    ownerId: v.id("users"),
+    dateCreated: v.number(),
+    lastUpdated: v.number()
+  })
 });
